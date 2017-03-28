10419197029a9b6144f435342dfd65632fb6af5e
NaGriS <rsdenissr@gmail.com>
2017-03-28 22:29:04 +0300

    Removed cache files

--- Database/create_database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-psql -c "CREATE DATABASE academicsvideo_db"
-psql -d academicsvideo_db -c "CREATE USER superuser WITH PASSWORD 'Password'"
-psql -d academicsvideo_db -c "ALTER ROLE superuser SET client_encoding TO 'utf8'"
-psql -d academicsvideo_db -c "ALTER ROLE superuser SET default_transaction_isolation TO 'read committed'"
-psql -d academicsvideo_db -c "ALTER ROLE superuser SET timezone TO 'UTC'"
-psql -d academicsvideo_db -c "GRANT ALL PRIVILEGES ON DATABASE academicsVideo_db TO superuser"

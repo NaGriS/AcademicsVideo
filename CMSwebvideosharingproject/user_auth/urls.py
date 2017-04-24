422bace949f2133fc618ce98e1d40cdc2d4abafb
NikitaShalyapin <cooluocker@yandex.ru>
2017-04-24 21:06:11 +0300

    merge develop

--- CMSwebvideosharingproject/user_auth/urls.py
+++ CMSwebvideosharingproject/user_auth/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
+    url(r'^$', views.redirect_to_login, name='redirect_to_login'),
     url(r'^register/$',views.UserFormView.as_view(),name='register'),
     url(r'^login_user/$', views.login_user, name='login_user'),
     url(r'^logout_user/$', views.logout_view, name='logout_user'),

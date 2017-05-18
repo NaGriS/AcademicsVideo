55443e64ccf436d97d272314d9a41e74fb03f371
langtucodon9stv <langtuphieudu.stv@gmail.com>
2017-05-18 20:41:49 +0100

    Merge branch 'develop' of https://github.com/NaGriS/AcademicsVideo into develop
    # Conflicts:
    #	CMSwebvideosharingproject/user_auth/forms.py
    #	CMSwebvideosharingproject/video_publishing/views.py
    comment complete + change password + reset password
    +delete comment in new project

--- CMSwebvideosharingproject/video_publishing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from video_publishing.models import Course_Create
-from video_publishing.models import Video_Create
-from .models import  Comment
-
-admin.site.register(Course_Create)
-admin.site.register(Video_Create)
-
-admin.site.register(Comment)

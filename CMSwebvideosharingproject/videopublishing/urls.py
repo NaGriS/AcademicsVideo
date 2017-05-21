070622baffcd5829e2436039108f25c10a5ddb4d
langtucodon9stv <langtuphieudu.stv@gmail.com>
2017-05-21 13:21:29 +0100

    Merge branch 'develop' of https://github.com/NaGriS/AcademicsVideo into develop
    # Conflicts:
    #	CMSwebvideosharingproject/user_auth/forms.py
    #	CMSwebvideosharingproject/video_publishing/views.py
    comment complete + change password + reset password
    +search+design+repair error

---
+++ CMSwebvideosharingproject/videopublishing/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from . import views
+from haystack.views import SearchView
+
+urlpatterns = [
+    url(r'^courses/(?P<pk>[0-9]+)/$', views.video_list, name='video_list'),
+    url(r'^courses/$', views.course_list, name='course_list'),
+    url(r'^courses/new/$', views.course_new, name='course_new'),
+    url(r'^courses/(?P<pk>[0-9]+)/edit/$', views.course_edit, name='course_edit'),
+    url(r'^courses/(?P<course_pk>[0-9]+)/delete/$', views.course_delete, name='course_delete'),
+    url(r'^courses/(?P<pk>[0-9]+)/new/$', views.video_new, name='video_new'),
+    url(r'^courses/(?P<course_pk>[0-9]+)/(?P<video_pk>[0-9]+)/$', views.video, name='video'),
+    url(r'^courses/(?P<course_pk>[0-9]+)/(?P<video_pk>[0-9]+)/edit/$', views.video_edit, name='video_edit'),
+    url(r'^courses/(?P<course_pk>[0-9]+)/(?P<video_pk>[0-9]+)/delete/$', views.video_delete, name='video_delete'),
+    url(r'^search/',include('haystack.urls')),
+]
